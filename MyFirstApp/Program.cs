0dab21261c9a6f99d5e4f9bb742a479ff3a6122f
DESKTOP-FUIPR8M\Марина <kpd.0000528@gmail.com>
2020-12-08 14:48:33 +0300

    first commit

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string word = "Hey you! What is you name??";
+            // Console.WriteLine(word);
+
+            //string answer = Console.ReadLine();
+            //Console.WriteLine("well, hello! " + answer);
+
+            //int x;
+            //x = 7;
+            //int x = 7;
+            //Console.WriteLine(x-3);
+            //x = 5;
+            //Console.WriteLine(x*10);
+            //Console.WriteLine(3+5);
+            //int x = 10;
+            //int y = 7;
+            //Console.WriteLine(x % y );
+
+           int a = int.Parse(Console.ReadLine());
+           int b = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine(a + b);
+            // Console.WriteLine(a - b);
+            // Console.WriteLine(a * b);
+            // Console.WriteLine(a / b);
+
+            string oper = Console.ReadLine();
+
+            if (oper == "+")
+
+                Console.WriteLine("a + b");
+
+            else if (oper == "-")
+
+                Console.WriteLine("a - b");
+
+            else if (oper == "a * b")
+
+                Console.WriteLine("a * b");
+
+            else if (oper == "a / b")
+
+                Console.WriteLine("a / b");
+
+            else
+
+                Console.WriteLine("wrong operator!");
+
+
+      
+
+
+
+           
+
+
+        }
+    }
+}
